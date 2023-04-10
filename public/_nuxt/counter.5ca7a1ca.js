9e1b71480ec408e0490aba80542a42a43a664483
ivanstrilets <45265680+ivanstrilets@users.noreply.github.com>
2023-04-10 17:36:01 +0000

    Deploying to gh-pages from @ vvebdev/nuxtjs@a7616c2455357979e8b7633f3121601c0331b266 🚀

---
+++ public/_nuxt/counter.5ca7a1ca.js
@@ -0,0 +1,1 @@
+import{k as t,r as n}from"./entry.30488190.js";const u=t("counter",()=>{const e=n(0);function r(){e.value++}return{count:e,increment:r}});export{u};
---
+++ public/_nuxt/test.d9b4091c.js
@@ -0,0 +1,1 @@
+import{h as t,a as e,o as s}from"./entry.30488190.js";const o={name:"test"};function n(a,c,r,p,_,f){return s(),e("h1",null,"test h1")}const m=t(o,[["render",n]]);export{m as default};
